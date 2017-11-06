901d1c424a1680a2779f1d153559faab4348a806
EkoAlex <alexandru@appticles.com>
2017-11-06 13:20:19 +0200

    Add main plugin file

---
+++ pwacommerce.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Plugin Name: PWAcommerce
+ * Description: Turn your E-commerce site into a Progressive Web App
+ * Author: pwacommerce.io
+ * Version: 0.1
+ */
+
+namespace PWAcommerce;
+
+require_once 'vendor/autoload.php';
+require_once 'core/config.php';
+
+global $pwacommerce_options;
+$pwacommerce_options = new Core\PWAcommerce();
+
+function PWAcommerce_admin_init() {
+	new Admin\Admin_Init();
+}
+
+function PWAcommerce_frontend_init() {
+	new Frontend\Frontend_Init();
+}
+
+register_activation_hook( __FILE__, [ &$pwacommerce, 'activate' ] );
+register_deactivation_hook( __FILE__, [ &$pwacommerce, 'deactivate' ] );
+
+if ( is_admin() ) {
+
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+
+		$pwacommerce_ajax = new Admin\Admin_Ajax();
+
+
+	} else {
+
+		add_action( 'plugins_loaded', 'PWAcommerce\pwacommerce_admin_init' );
+	}
+
+} else {
+
+	add_action( 'plugins_loaded', 'PWAcommerce\pwacommerce_frontend_init' );
+}
+
