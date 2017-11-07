7fc19c32e7b3457e8f76e28cf308e6c2262ec3a6
EkoAlex <alexandru@appticles.com>
2017-11-07 12:21:47 +0200

    Fix typo

--- admin/js/UI.Modules/PWAcommerce_Settings.js
+++ admin/js/UI.Modules/PWAcommerce_Settings.js
@@ -78,12 +78,12 @@ function PWACOMMERCE_SETTINGS() {
       // set the value on the dummy text field that will always be visible in the post data
       if (this.checked) {
         jQuery(
-          '#' + JSObject.type + 'service_worker_installed',
+          '#' + JSObject.type + '_service_worker_installed',
           JSObject.DOMDoc
         ).val('1');
       } else {
         jQuery(
-          '#' + JSObject.type + 'service_worker_installed',
+          '#' + JSObject.type + '_service_worker_installed',
           JSObject.DOMDoc
         ).val('0');
       }
