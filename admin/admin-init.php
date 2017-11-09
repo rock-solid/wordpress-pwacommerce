f7fbcfab771e36a2b84ccffdca3641fd09db0f6c
EkoAlex <alexandru@appticles.com>
2017-11-09 12:45:36 +0200

    Enqueue upload icon js files

--- admin/admin-init.php
+++ admin/admin-init.php
@@ -69,7 +69,7 @@ class Admin_Init {
 		wp_enqueue_script( $pwacommerce_options->prefix . 'js_interface', plugins_url( PWACOMMERCE_DOMAIN . '/admin/js/UI.Interface/JSInterface.min.js' ), $dependencies, PWACOMMERCE_VERSION );
 		wp_enqueue_script( $pwacommerce_options->prefix . 'js_settings', plugins_url( PWACOMMERCE_DOMAIN . '/admin/js/UI.Modules/PWAcommerce_Settings.min.js' ), [], PWACOMMERCE_VERSION );
 		wp_enqueue_script( $pwacommerce_options->prefix . 'js_subscribe', plugins_url( PWACOMMERCE_DOMAIN . '/admin/js/UI.Modules/PWAcommerce_Subscribe.min.js' ), [], PWACOMMERCE_VERSION );
-
+		wp_enqueue_script( $pwacommerce_options->prefix . 'js_editimages', plugins_url( PWACOMMERCE_DOMAIN . '/admin/js/UI.Modules/PWAcommerce_EditImages.min.js' ), [], PWACOMMERCE_VERSION );
 	}
 
 }
