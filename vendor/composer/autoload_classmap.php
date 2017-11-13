bfc3ce93a8b895261ab62c9f109e743cfc1ddd18
EkoAlex <alexandru@appticles.com>
2017-11-13 11:14:48 +0200

    Add mobiledetect package

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,9 +6,13 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Mobile_Detect' => $vendorDir . '/mobiledetect/mobiledetectlib/Mobile_Detect.php',
     'PWAcommerce\\Admin\\Admin_Ajax' => $baseDir . '/admin/class-admin-ajax.php',
     'PWAcommerce\\Admin\\Admin_Init' => $baseDir . '/admin/admin-init.php',
     'PWAcommerce\\Core\\PWAcommerce' => $baseDir . '/core/class-pwacommerce.php',
+    'PWAcommerce\\Frontend\\Detect' => $baseDir . '/frontend/class-detect.php',
+    'PWAcommerce\\Frontend\\Frontend_Init' => $baseDir . '/frontend/frontend-init.php',
+    'PWAcommerce\\Includes\\Cookie' => $baseDir . '/includes/class-cookie.php',
     'PWAcommerce\\Includes\\Options' => $baseDir . '/includes/class-options.php',
     'PWAcommerce\\Includes\\PWAcommerce_API' => $baseDir . '/includes/class-pwacommerce-api.php',
     'PWAcommerce\\Includes\\Uploads' => $baseDir . '/includes/class-uploads.php',
