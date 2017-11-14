65198a6fcb40c9733f829b6cd3e7326b62e46e89
EkoAlex <alexandru@appticles.com>
2017-11-14 13:57:35 +0200

    Add redirects for product and category pages

---
+++ frontend/themes/app/single-product.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+if (is_numeric(get_the_ID())){
+    header("Location: ".home_url()."/#/product/".get_the_ID());
+} else {
+    header("Location: ".home_url());
+}
---
+++ frontend/themes/app/taxonomy-product_cat.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$obj = get_queried_object();
+$id = $obj->term_id;
+
+if ( is_numeric($id) ){
+    header("Location: ".home_url()."/#/category/". $id );
+} else {
+    header("Location: ".home_url());
+}
